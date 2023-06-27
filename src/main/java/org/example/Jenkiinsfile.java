58d6fe6128974f1750c2aa737007a0db3663d529
Abhijit Hanumale <Abhijit.Hanumale@harbingergroup.com>
2023-06-27 12:52:29 +0530

    all done

--- src/main/java/org/example/Jenkiinsfile.java
+++ src/main/java/org/example/Jenkiinsfile.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Jenkiinsfile {
     public static void main(String[] args) {
-        System.out.println("sjenkins pipeline");
+        System.out.println("sjenkins pipeline 3");
     }
 
 }
